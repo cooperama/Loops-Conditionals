4387c703c5223a9906a5cc492d29d1a162d42138
cooperama <coope133@gmail.com>
2020-09-10 16:57:05 -0700

    Commit 1 - Easy Going answered

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+console.log('Loops && Conditionals')
+
+
+// Easy Going
+console.log('~~~~~')
+console.log('Easy Going')
+console.log('~~~~~')
+
+for (let i = 1; i <= 20; i++) {
+  console.log(i);
+}
+
