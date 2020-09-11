679cd46e3a86509c250250735435a9139bc7be1f
cooperama <coope133@gmail.com>
2020-09-10 17:45:31 -0700

    Commit 3b - Added random cat messages

--- scripts/app.js
+++ scripts/app.js
@@ -24,8 +24,18 @@ console.log('~~~~~')
 console.log('3. Excited Kitten')
 console.log('~~~~~')
 
+let random = ['...human...why you taking pictures of me?...', '...the catnip made me do it...', '...why does the red dot always get away'];
 for (let i = 0; i < 20; i++) {
+  if (i % 2 === 0) {
+    let randomIndex = Math.floor(Math.random() * random.length)
+    console.log(random[randomIndex]);
+  }
   console.log('Love me, pet me! HSSSS!');
 }
 
+
+// 4. Fizz Buzz
+console.log('~~~~~')
+console.log('4. Fizz Buzz')
+console.log('~~~~~')
 
