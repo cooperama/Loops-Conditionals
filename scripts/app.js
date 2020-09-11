3b7f2ef11446e56259194bef0e00876e91ad238b
cooperama <coope133@gmail.com>
2020-09-10 17:38:09 -0700

    Commit 2 - Get Even answered

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,21 @@
 console.log('Loops && Conditionals')
 
 
-// Easy Going
+// 1. Easy Going
 console.log('~~~~~')
-console.log('Easy Going')
+console.log('1. Easy Going')
 console.log('~~~~~')
 
 for (let i = 1; i <= 20; i++) {
+  console.log(i);
+}
+
+// 2. Get Even
+console.log('~~~~~')
+console.log('2. Get Even')
+console.log('~~~~~')
+
+for (let i = 0; i <= 200; i+=2) {
   console.log(i);
 }
 
